a8fd6eb8ab26bb6ccde8dac5a87094bd5e5e75cb
dtranvan <dtranvan@avaya.com>
2024-06-10 10:07:34 +0700

    update

--- codility-1/src/main/java/org/example/Main.java
+++ codility-1/src/main/java/org/example/Main.java
@@ -1,11 +1,48 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int[] A = {3, 8, 9, 7, 6};
+        int K = 3;
+
+        //System.out.println(Arrays.toString(solution(A, K)));
     }
 
-    public static int solution(int N) {
+
+
+    public static int[] solution2(int[] A, int K) {
+        int length = A.length;
+        if (length == 0 || K <= 0) {
+            return A;
+        }
+
+        java.util.LinkedList<Integer> list = new java.util.LinkedList<>();
+        for (int i : A) {
+            list.push(i);
+        }
+
+        K = K % length;
+
+        for (int i = 0; i < K; i++) {
+            Integer item = list.pop();
+            list.addLast(item);
+        }
+
+
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            A[i] = list.get(size - i - 1);
+        }
+
+        return A;
+    }
+
+
+    public static int solution1(int N) {
         // Implement your solution here
         if (N < 1 || N > 2147483647) {
             return 0;
